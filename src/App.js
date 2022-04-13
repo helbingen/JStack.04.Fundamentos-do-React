86825ec278e088ccbccb53bbbde094ea6e47cc05
Daniel Helbingen <danielhelb@hotmail.com>
2022-04-12 22:20:21 -0300

    commit final

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import ThemeContextProvider from './context/ThemeContext';
+
+import GlobalStyle from './styles/global';
+import Layout from './components/Layout';
+
+
+function App() {
+  return (
+    <ThemeContextProvider>
+        <GlobalStyle />
+        <Layout />
+    </ThemeContextProvider>
+  );
+};
+
+export default App;
